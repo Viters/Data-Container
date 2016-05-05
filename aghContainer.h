6f8b03a569203c930600508cd21a507c987e688c
Łukasz Szcześniak <sir.viters@gmail.com>
2016-05-05 18:36:28 +0200

    Dodałem opisy metod.

--- aghContainer.h
+++ aghContainer.h
@@ -1,6 +1,11 @@
-//
-// Created by sir.viters on 2016-05-05.
-//
+/**
+ * \file aghContainer.h
+ * \author £ukasz Mielczarek, £ukasz Szczeœniak
+ * \date 05|05|2016
+ * \version 0.1
+ * \brief Contains declaration and definitions of aghContainer template class.
+ */
+// --------------------------------------------------------------------------------
 
 #ifndef DATA_CONTAINER_AGHCONTAINER_H
 #define DATA_CONTAINER_AGHCONTAINER_H
@@ -9,45 +14,219 @@
 
 using namespace std;
 
-template<typename T>
+/**
+ * Class template for data containers.
+ */
+template <typename T>
 class aghContainer {
 public:
-    virtual void append(T const &) = 0;
+    /**
+	 * Pure virtual destructor.
+	 */
+    virtual ~aghContainer() = 0;
 
-    virtual bool insert(const int, T const &) = 0;
+    /**
+	 * Add element to container.
+	 * @param new element
+	 */
+    virtual void append(T const&) = 0;
 
-    virtual bool replace(const int, T const &) = 0;
+    /**
+	 * Join two containers of the same type.
+	 * @param another container
+	 */
+    virtual void append(aghContainer<T> const& right) = 0;
 
-    virtual T &at(const int) const = 0;
+    /**
+	 * Insert an element at specified position.
+	 * @param position number
+	 * @param new element
+	 */
+    virtual bool insert(const int, T const&) = 0;
 
+    /**
+	 * Replace an element at specified position.
+	 * @param position number
+	 * @param new element
+	 */
+    virtual bool replace(const int, T const&) = 0;
+
+    /**
+	 * Get element at specified position.
+	 * @param position number
+	 */
+    virtual T& at(const int) const = 0;
+
+    /**
+	 * Get size of the container.
+	 * @return int size
+	 */
     virtual int size(void) const = 0;
 
+    /**
+	 * Remove element at specified position.
+	 * @param position number
+	 * @return bool
+	 */
     virtual bool remove(const int) = 0;
 
+    /**
+	 * Remove all elements from the container.
+	 */
     virtual void clear(void) = 0;
 
+    /**
+	 * Check if there are any elements in container.
+	 * 
+	 * @return bool
+	 */
     virtual bool isEmpty(void) = 0;
 
-    virtual int indexOf(T const &_value, int _from = 0) const = 0;
+    /**
+     * Get index of element in container within specified range.
+     *
+     * @param element value
+     * @param start index (default = 0)
+     * @return int index
+     */
+    virtual int indexOf(T const& _value, int _from = 0) const = 0;
 
-    virtual bool contains(T const &_value, int _from = 0) const = 0;
+    /**
+     * Check if element exists in container within specified range.
+     *
+     * @param element value
+     * @param start index (default = 0)
+     * @return bool
+     */
+    virtual bool contains(T const& _value, int _from = 0) const = 0;
 
-    virtual bool operator==(aghContainer<T> const& right) = 0;
+    /**
+     * Check if two containers are the same.
+     *
+     * @param another container
+     * @return bool
+     */
+    virtual bool equal(aghContainer<T> const& right) const = 0;
 
-    virtual bool operator!=(aghContainer<T> const& right) = 0;
+    /**
+     * Operator to check if two containers are the same.
+     *
+     * @param another container
+     * @return bool
+     */
+    virtual bool operator==(aghContainer<T> const& right) const;
 
-    virtual T& operator[](int n) const = 0;
+    /**
+     * Operator to check if two containers are not the same.
+     *
+     * @param another container
+     * @return bool
+     */
+    virtual bool operator!=(aghContainer<T> const& right) const;
 
-    virtual aghContainer<T>& operator+=(aghContainer<T> const& right) = 0;
+    /**
+     * Operator to get element from specified position.
+     *
+     * @param position number
+     * @return element
+     */
+    virtual T& operator[](const int n) const;
 
-    virtual aghContainer<T>& operator+=(T const& element) = 0;
+    /**
+     * Operator to add element at specified position.
+     *
+     * @param new element
+     * @return reference to itself
+     */
+    virtual aghContainer<T>& operator+=(T const& element);
 
-    virtual aghContainer<T>& operator<<(T const& element) = 0;
+    /**
+     * Operator to join two containers of the same type.
+     *
+     * @param another container
+     * @return reference to itself
+     */
+    virtual aghContainer<T>& operator+=(aghContainer<T> const& right);
 
-    virtual aghContainer<T>& operator<<(aghContainer<T> const& right) = 0;
+    /**
+     * Operator to add element at specified position.
+     *
+     * @param new element
+     * @return reference to itself
+     */
+    virtual aghContainer<T>& operator<<(T const& element);
 
-    virtual ostream& operator<<(ostream&, aghContainer<T> const& right) = 0;
+    /**
+     * Operator to join two containers of the same type.
+     *
+     * @param another container
+     * @return reference to itself
+     */
+    virtual aghContainer<T>& operator<<(aghContainer<T> const& right);
+
+    /**
+     * Operator to print container elements.
+     *
+     * @param container
+     * @return reference to ostream object
+     */
+	virtual ostream& operator<<(ostream&, aghContainer<T> const& right) = 0;
 };
+// --------------------------------------------------------------------------------
+// End of aghContainer declaration.
+// --------------------------------------------------------------------------------
 
+template <typename T>
+bool aghContainer<T>::operator==(aghContainer<T> const& right) const {
+	return this->equal(right);
+}
+
+// --------------------------------------------------------------------------------
+
+template <typename T>
+bool aghContainer<T>::operator!=(aghContainer<T> const& right) const {
+	return !(this->equal(right));
+}
+
+// --------------------------------------------------------------------------------
+
+template <typename T>
+T& aghContainer<T>::operator[](const int n) const {
+	return this->at(n);
+}
+
+// --------------------------------------------------------------------------------
+
+template <typename T>
+aghContainer<T>& aghContainer<T>::operator+=(T const& element) {
+    this->append(element);
+    return *this;
+}
+
+// --------------------------------------------------------------------------------
+
+template <typename T>
+aghContainer<T>& aghContainer<T>::operator+=(aghContainer<T> const& right) {
+	this->append(right);
+    return *this;
+}
+
+// --------------------------------------------------------------------------------
+
+template <typename T>
+aghContainer<T>& aghContainer<T>::operator<<(T const& element) {
+	this->append(element);
+    return *this;
+}
+
+// --------------------------------------------------------------------------------
+
+template <typename T>
+aghContainer<T>& aghContainer<T>::operator<<(aghContainer<T> const& right) {
+	this->append(right);
+    return *this;
+}
+
+// --------------------------------------------------------------------------------
 
 #endif //DATA_CONTAINER_AGHCONTAINER_H
