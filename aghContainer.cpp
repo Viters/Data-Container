bdbb924810d934c870f3757413d727eb72040ebd
Łukasz Szcześniak <sir.viters@gmail.com>
2016-05-05 12:48:53 +0200

    Stworzyłem klasę aghContainer

---
+++ aghContainer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sir.viters on 2016-05-05.
+//
+
+#include "aghContainer.h"
---
+++ aghContainer.h
@@ -0,0 +1,14 @@
+//
+// Created by sir.viters on 2016-05-05.
+//
+
+#ifndef DATA_CONTAINER_AGHCONTAINER_H
+#define DATA_CONTAINER_AGHCONTAINER_H
+
+
+class aghContainer {
+
+};
+
+
+#endif //DATA_CONTAINER_AGHCONTAINER_H
