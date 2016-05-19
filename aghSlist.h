5ce6990c6d864a70205ee6fd9bf1d88205e97d2f
Lukasz Szczesniak <sir.viters@gmail.com>
2016-05-19 10:05:59 +0200

    Stworzyłem zarys klasy aghSlist

---
+++ aghSlist.h
@@ -0,0 +1,32 @@
+/**
+ * @file aghSlist.h
+ * @author Łukasz Mielczarek, Łukasz Szcześniak
+ * @date 19|05|2016
+ * @version 0.1
+ * @brief Contains declaration and definition of aghSlist template class.
+*/
+
+// --------------------------------------------------------------------------------
+
+#ifndef DATA_CONTAINER_AGHSLIST_H
+#define DATA_CONTAINER_AGHSLIST_H
+
+#include "aghContainer.h"
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Class template to provide operation on single-sided list.
+*/
+template<typename T>
+class aghVector :
+	public aghContainer<T> {
+public:
+
+
+private:
+
+};
+
+#endif
