669925d459a6ff211478078d854793d74ef9c1ed
Łukasz Szcześniak <sir.viters@gmail.com>
2016-06-01 21:40:45 +0200

    Dodałem wstępną deklarację aghIterator.

---
+++ aghIterator.h
@@ -0,0 +1,30 @@
+/**
+ * @file aghIterator.h
+ * @author Łukasz Mielczarek, Łukasz Szcześniak
+ * @date 01/06/2016
+ * @version 0.1
+ * @brief Contains declaration and definitions of aghIterator template class.
+*/
+
+// --------------------------------------------------------------------------------
+
+#ifndef DATA_CONTAINER_AGHITERATOR_H
+#define DATA_CONTAINER_AGHITERATOR_H
+
+/**
+ * @class aghIterator
+ *
+ * @brief Class template to provide aghContainer iterators.
+ *
+ * @author Łukasz Mielczarek, Łukasz Szcześniak
+ *
+ * @version 0.1
+ *
+ * @date 01/06/2016
+ */
+class aghIterator {
+
+};
+
+
+#endif //DATA_CONTAINER_AGHITERATOR_H
